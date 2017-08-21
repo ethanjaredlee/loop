28281ddd27468bba6f3bc927f0ed466a1baed24e
ethlee4 <ethanlee2020@u.northwestern.edu>
2017-08-20 22:47:52 -0500

    onlcick add more bows

--- loop.js
+++ loop.js
@@ -11,9 +11,7 @@ function draw() {
     background(101);
     noFill();
 
-    for (i=0; i<number; i++) {
-        bow = new Bow();
-        bows.push(bow);
+    for (i=0; i<bows.length; i++) {
         bows[i].display(i);
     }
 }
@@ -28,16 +26,24 @@ function Bow() {
     this.rev = 0;
 
     this.display = function(r) {
+        console.log(this.rev);
         end = map(sin(toRadians(this.lifespan)), -1, 1, 0, TWO_PI);
         strokeWeight(5);
+        stroke(map(end, 0, TWO_PI, 0, 255));
         if (end<PI) {
             arc(width/2, height/2, r*50, r*50, end, PI);
         } else if (end > PI) {
             arc(width/2, height/2, r*50, r*50, PI, end);
-        } else if (end == 0) {
+        }
+        if (abs(end-PI)<.01) {
             this.rev++;
         }
         this.lifespan++;
     }
 
+}
+
+function mouseClicked() {
+    bow = new Bow();
+    bows.push(bow);
 }
