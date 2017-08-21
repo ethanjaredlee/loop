348ba3a8661b3e40d7d352f4a9cb30e1b5698628
ethlee4 <ethanlee2020@u.northwestern.edu>
2017-08-20 22:18:34 -0500

    abstracted to a class

--- loop.js
+++ loop.js
@@ -4,6 +4,7 @@ function setup() {
     createCanvas(600, 400);
     count = 1;
     number = 5;
+    bows = [];
 }
 
 function draw() {
@@ -11,18 +12,32 @@ function draw() {
     noFill();
 
     for (i=0; i<number; i++) {
-        end = map(sin(toRadians(count)), -1, 1, 0, TWO_PI);
-        if (end<PI) {
-            arc(width/2, height/2, i*50, i*50, end, PI);
-        } else if (end > PI) {
-            arc(width/2, height/2, i*50, i*50, PI, end);
-        }
+        bow = new Bow();
+        bows.push(bow);
+        bows[i].display(i);
     }
-    count++;
 }
 
 function toRadians(degrees) {
     return degrees * TWO_PI / 360;
 }
 
+function Bow() {
 
+    this.lifespan = 0;
+    this.rev = 0;
+
+    this.display = function(r) {
+        end = map(sin(toRadians(this.lifespan)), -1, 1, 0, TWO_PI);
+        strokeWeight(5);
+        if (end<PI) {
+            arc(width/2, height/2, r*50, r*50, end, PI);
+        } else if (end > PI) {
+            arc(width/2, height/2, r*50, r*50, PI, end);
+        } else if (end == 0) {
+            this.rev++;
+        }
+        this.lifespan++;
+    }
+
+}
